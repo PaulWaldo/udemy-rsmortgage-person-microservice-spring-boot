197c2228b9749f5d2c6590951f5712908b10eab8
Paul Waldo <paulwwaldo@gmail.com>
2017-07-31 13:05:55 -0400

    Create Person, PersonRepository, PersonServiceEvent from template

---
+++ src/main/java/com/rollingstone/dao/jpa/PersonRepository.java
@@ -0,0 +1,16 @@
+package com.rollingstone.dao.jpa;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import com.rollingstone.domain.Person;
+
+public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
+
+	List<Person> getByLastName(String lastName);
+	List<Person> getByFirstName(String firstName);
+	Page<Person> findAll(Pageable pageable);
+}
